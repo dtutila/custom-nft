30432da57a17cbb9262bca556d168b885275e462
dtutila <daniel@dtutila.com>
2024-04-21 20:51:12 -0600

    initial tests

---
+++ test/MoodNFTTest.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.25;
+
+import {Test} from "forge-std/Test.sol";
+import {DeployMoodNFT} from "../script/DeployMoodNFT.s.sol";
+import {MoodNFT} from "../src/MoodNFT.sol";
+
+contract MoodNFTTest is Test {}
