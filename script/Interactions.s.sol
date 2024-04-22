16acbdefc369a1d06b1d1d0bc3ec82b9bfc22cbc
dtutila <daniel@dtutila.com>
2024-04-21 20:51:30 -0600

    initial mood scripts

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 import {Script} from "forge-std/Script.sol";
-import {DeployBasicNFT} from "./DeployBasicNFT.s.sol";
+// import {DeployBasicNFT} from "./DeployBasicNFT.s.sol";
+// import {DeployMoodNFT} from "./DeployMoodNFT.s.sol";
 import {BasicNFT} from "../src/BasicNFT.sol";
+import {MoodNFT} from "../src/MoodNFT.sol";
 import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 
 contract MintBasicNFT is Script {
@@ -16,6 +18,23 @@ contract MintBasicNFT is Script {
 
         vm.broadcast();
         BasicNFT(contractAddress).mintNFT(STRING_URI);
+        vm.stopBroadcast;
+        
+    }
+
+}
+
+contract MintMoodNFT is Script {
+    
+    function run() external returns (MoodNFT) {
+       address mostRecentMoodNFT = DevOpsTools.get_most_recent_deployment("MoodNFT", block.chainid);
+        return mintNFTOnContract(mostRecentMoodNFT);
+    }
+
+    function mintNFTOnContract(address contractAddress) public returns (MoodNFT){
+
+        vm.broadcast();
+        MoodNFT(contractAddress).mintNFT();
         vm.stopBroadcast;
         
     }
