06c5e5299a04a18f472c32f2f125a08d8daaaead
dtutila <daniel@dtutila.com>
2024-04-21 20:50:58 -0600

    added mood nft

--- src/MoodNFT.sol
+++ src/MoodNFT.sol
@@ -3,11 +3,63 @@
 pragma solidity ^0.8.25;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract MoodNFT is ERC721 {
+    uint256 private s_tokenCounter;
+    string private s_happyImageURI;
+    string private s_sadImageURI;
+    mapping(uint256 => Mood) s_tokenIdToMood;
+    
+    enum Mood {
+        HAPPY,
+        SAD
+    }
 
-    constructor () ERC721("Mood", "MN") {
 
+    constructor (
+        string memory _happyImageURI,
+        string memory _sadImageURI
+    ) ERC721("Mood", "MN") {
+        s_happyImageURI = _happyImageURI;
+        s_tokenIdToMood[s_tokenCounter] = Mood.HAPPY;
+        s_sadImageURI = _sadImageURI;
+
+
+    }
+    function mintNFT() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64";
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+    
+        string memory imageURI = s_happyImageURI;
+
+        if (s_tokenIdToMood[tokenId] == Mood.SAD) {
+            imageURI = s_sadImageURI;
+        }
+        return string(
+            abi.encodePacked(
+                _baseURI(),
+                Base64.encode(
+                    bytes( 
+                        abi.encodePacked(
+                            '{"name":"',
+                            name(), 
+                            '", "description":"An NFT that reflects the mood of the owner, 100% on Chain!", ',
+                            '"attributes": [{"trait_type": "moodiness", "value": 100}], "image":"',
+                            imageURI,
+                            '"}'
+                        )
+                    )
+                )
+            )
+        );
     }
 
 }
