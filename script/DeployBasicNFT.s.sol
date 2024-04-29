a085565e07a2ca49b131d1b888c0b758373c8b47
dtutila <daniel@dtutila.com>
2024-04-28 20:09:46 -0600

    removed svg from code
    clean up

--- script/DeployBasicNFT.s.sol
+++ script/DeployBasicNFT.s.sol
@@ -1,17 +1,20 @@
-// SPDX-License-IdentifierP: MIT
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.25;
+
 
 import {Script} from "forge-std/Script.sol";
 import {BasicNFT} from "../src/BasicNFT.sol";
 
+
 contract DeployBasicNFT is Script {
+
     function run() external returns (BasicNFT) {
         BasicNFT bnft;
         vm.startBroadcast();
         bnft = new BasicNFT();
+        
         vm.stopBroadcast();
-
         return bnft;
     }
 }
